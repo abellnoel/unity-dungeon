f103590add923290e8fed9ccdf7589007ab3d0f5
Trevor Hedgpeth <trevorthebeast1217@gmail.com>
2020-05-25 15:01:51 -0400

    Add health script

---
+++ Unity Dungeon/Assets/Scripts/Health.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    const int DEFAULT_HP = 20;
+    int health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = DEFAULT_HP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int getHealth(){
+        return health;
+    }
+}
