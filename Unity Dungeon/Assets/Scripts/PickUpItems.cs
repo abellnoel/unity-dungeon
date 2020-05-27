de026cfb019ec1f901d3a2967015efc4c0d38fc6
Trevor Hedgpeth <trevorthebeast1217@gmail.com>
2020-05-27 19:00:47 -0400

    Add inventory system and health pickup/button

---
+++ Unity Dungeon/Assets/Scripts/PickUpItems.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItems : MonoBehaviour
+{
+    private Inventory inventory;
+    public GameObject itemButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            for (int i = 0; i < inventory.slots.Length; i++) //Checking to see if the inventory is empty
+            {
+                if (inventory.isFull[i] == false)
+                { //Means item can be picked up
+                    inventory.isFull[i] = true;
+                    Instantiate(itemButton, inventory.slots[i].transform, false);
+                    Destroy(gameObject); //Destroys the pick up from the world
+                    break;
+                }
+            }
+        }
+    }
+}
