f0540db7cbb33ae71a339d9fae009cd1db9f039f
Trevor Hedgpeth <trevorthebeast1217@gmail.com>
2020-05-28 18:59:21 -0400

    Add new scene and faulty scene transitioner
    I added another scene (Scene2), a script (SceneChange), added a player,
    and added an invisible collider mesh in scene one
    that should change the scene. I can tell by the console that
    a collision is detected between player and the mesh,
    however, the scene does not change.

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  //Need this
+
+public class ChangeScene : MonoBehaviour
+{
+    public string sceneToLoad;
+   void OnTriggerEnter(Collider collision) {
+       if(collision.CompareTag("Player") && !collision.isTrigger){
+           SceneManager.LoadScene(sceneToLoad);
+       }
+   }
+   /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+}
