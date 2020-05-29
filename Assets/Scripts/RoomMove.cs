aca0ce5be0cddeebec9104869ca75fe057d29283
Trevor Hedgpeth <trevorthebeast1217@gmail.com>
2020-05-29 18:06:19 -0400

    Add new room with camera transition and Prefabs
    I added a second tilemap with the same features as the first
    (Room1 and Room2). I then made a script to transition the
    player and the camera into the second room when a trigger
    is hit. I then figured out prefabs and made a mutable
    prefab folder and added the trigger into it. There are
    tons of things we need to go back to and put them into
    the prefabs because I just learned how it works and why
    it is helpful.

---
+++ Assets/Scripts/RoomMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomMove : MonoBehaviour
+{
+    /*
+    This class is just going to make the camera transition
+    from one room to the next smoothly. This is not changing
+    the scene, I just made a seperate tilemap and connected
+    them for this.
+
+    Also note that this only works for square rooms, anything
+    else would require another vector for change
+    */
+    public Vector2 cameraChange;
+    public Vector3 playerChange;
+    private CameraMovement cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.GetComponent<CameraMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")){
+            cam.minPosition += cameraChange;
+            cam.maxPosition += cameraChange;
+            other.transform.position += playerChange; //Moving the player where they need to be
+        }
+    }
+}
