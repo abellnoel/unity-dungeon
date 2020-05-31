005cc5040c9e24910478d64ebd0c3efed74186b7
Trevor Hedgpeth <trevorthebeast1217@gmail.com>
2020-05-30 20:28:19 -0400

    Add new menu Scene
    I repurposed the second scene to make a menu.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame(){//Basically starts the game
+        SceneManager.LoadScene(SceneManager.GetActiveScene()
+                               .buildIndex + 1); //Getting the next
+                                                 // Scene in the queue
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
