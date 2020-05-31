6a1315907dd1554b2fb9592303ec717d8668cce9
Trevor Hedgpeth <trevorthebeast1217@gmail.com>
2020-05-30 21:56:48 -0400

    Add two new scene transitions
    I added a simple transition called crossfade
    Then I tried making my own with placeholder
    art called squarewipe. It works but it isnt
    polished. I just need the art department to
    make something........... if that ever happens

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            LoadNextLevel();
+        }
+    }
+    public void LoadNextLevel(){
+        StartCoroutine(LoadScene(SceneManager.GetActiveScene()
+                               .buildIndex + 1));
+        
+    }
+
+    IEnumerator LoadScene(int sceneIndex){
+        //Play animation
+        transition.SetTrigger("Start");
+        //Wait
+        yield return new WaitForSeconds(transitionTime);
+        //Load Scene    
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
