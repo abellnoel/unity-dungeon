8ae205046594afd7f97e6f6f44680a3fe10fd706
nabellan1 <59884349+nabellan1@users.noreply.github.com>
2020-05-31 20:09:30 -0400

    Add IceMage movement animations to Player
    Added various IceMage animation clips and the IceMage animation
    controller. Controller determines current horizonal input and saves it
    into the lastDirection variable. This variable is only updated when the
    player is moving horizontally in some way. This is used to keep facing
    previous direction when idling or only moving vertically.

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -6,8 +6,10 @@ public class movement : MonoBehaviour
 {
     //variables
     public Rigidbody2D rb; 
+    public float moveSpeed = 5f;
+    public Animator animator;
     private Vector2 move;
-    public float moveSpeed = 5f; 
+    private float lastDirection = 1;
 
     // Update is called once per frame
     void Update()
@@ -15,6 +17,13 @@ public class movement : MonoBehaviour
         //between -1 and 1
         move.x = Input.GetAxisRaw("Horizontal");
         move.y = Input.GetAxisRaw("Vertical");
+
+        //update animator values
+        if (move.x != 0)
+            lastDirection = move.x;
+        animator.SetFloat("lastDirection", lastDirection);
+        animator.SetFloat("isMoving", move.sqrMagnitude); //not entirely sure how this works
+
     }
 
     // Called once per fixed amount of time, more consistent for physics
