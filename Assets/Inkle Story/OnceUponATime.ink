8532059a43572876c421e94d9c2e7a99770016f5
Trevor Hedgpeth <trevorthebeast1217@gmail.com>
2020-06-12 21:14:45 -0400

    Add example scene with branching text
    Add ink add on with ink story, added an example
    scene for later reference on how to make text
    with branching options. A lot of work went into
    trying to copy the tutorial I was following
    but i still dont really understand a lot of it.
    It is mainly for us to reference later.

---
+++ Assets/Inkle Story/OnceUponATime.ink
@@ -0,0 +1,23 @@
+Practice
+by
+Trevor Hedgpeth
+
+* [Begin] ->FirstDialog
+
+== FirstDialog ==
+Once upon a time noel and his freind Austin were bitches
+
+These bitches be crazy
+
+* [They really do be] "You right cuz.." -> TheyCrazy
+
+* [bitches dont be that crazy] "You trippin cuz.." -> TheyAint
+
+== TheyCrazy ==
+Bitches be trippin
+-> END
+
+== TheyAint ==
+You wrong fa dat -> END
+
+-> END
---
+++ Assets/Scripts/StoryControl.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ink.Runtime;
+using TMPro;
+using UnityEngine.UI;
+
+public class StoryControl : MonoBehaviour
+{
+    //public static event Action<Story> OnCreateStory;
+    [SerializeField]
+    private TextAsset inkStory;
+
+    public Story story;
+
+    public TextMeshProUGUI storyText;
+
+    public TextMeshProUGUI[] buttonText;
+    public Button[] buttons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartStory();
+    }
+    void StartStory(){
+        story = new Story(inkStory.text);
+        UpdateStory();
+    }
+
+    void UpdateStory(){ //Should be UpdateStory
+        if(story != null){
+            int temp = 0;
+            string currentStory = "";
+            while(story.canContinue && temp < 100){
+                story.Continue();
+                temp++;
+                currentStory += story.currentText;
+                storyText.text = currentStory;
+            }
+            if(story.currentChoices.Count > 0){
+                MakeChoices();
+            }
+            else{
+                //storyText.text = "End of Story. \n Restart?";
+                buttonText[0].text = "Restart";
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void MakeChoices(){
+        for(int i = 0; i < buttonText.Length; i++){
+
+            if(i < story.currentChoices.Count){
+                //buttons[i].enabled = true;
+                buttonText[i].text = story.currentChoices[i].text;
+            }
+            else{
+                buttonText[i].text = "...";
+                buttons[i].enabled = false;
+            }
+        }
+    }
+
+    public void Choice(int choiceNumber){
+        story.ChooseChoiceIndex(choiceNumber);
+        UpdateStory(); // updating story
+    }
+}
