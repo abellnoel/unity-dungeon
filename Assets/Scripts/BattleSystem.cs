3f0be802813d1fa549acfd5d51fc8a1799d5837e
Trevor Hedgpeth <trevorthebeast1217@gmail.com>
2020-06-14 21:40:57 -0400

    Add HUD functionalies to turn based combat

---
+++ Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleSystem : MonoBehaviour
+{
+    public BattleHUD playerHUD;
+    public BattleHUD enemyHUD;
+
+    public Text dialogText;
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    Unit playerUnit;
+    Unit enemyUnit;
+
+    public Transform playerBattleStation;
+    public Transform enemyBattleStation;
+
+    public BattleState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+        SetupBattle();
+    }
+    void SetupBattle(){
+        GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
+        playerUnit = playerGO.GetComponent<Unit>();
+
+        GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
+        enemyUnit = enemyGO.GetComponent<Unit>();
+
+        dialogText.text = "You see a " +enemyUnit.unitName + " get into battle stance..";
+
+        playerHUD.SetHUD(playerUnit);
+        enemyHUD.SetHUD(enemyUnit);
+    }
+
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public string unitName;
+    public int unitLevel; //Maybe
+
+    public int damage;
+
+    public int maxHP;
+    public int currentHP;
+}
