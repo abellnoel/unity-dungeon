4ef9c1736830b32b4c0e6f8bc4d4c1a6374aacc2
Trevor Hedgpeth <trevorthebeast1217@gmail.com>
2020-06-22 09:31:09 -0400

    Add new enemy AI

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour //AI for every enemy
+{
+    public int health;
+    public float moveSpeed;
+    public string enemyName;
+    public int baseAttack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Log.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : EnemyAI //inheritance of enemyAI
+{
+    public Transform target;
+    public float chaseRadius; //if player is close enough it will chase
+    public float attackRadius; //attack the player
+    public Transform homePosition;//Where the enemy goes back to
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform; //gets location of player
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckDistance();
+    }
+
+    void CheckDistance(){
+        if(Vector3.Distance(target.position, transform.position)
+                             <= chaseRadius
+        && Vector3.Distance(target.position, transform.position)
+                             > attackRadius){ 
+                                 
+        /*This ^^ checks the distance between player and the log
+        to see if the log should chase the enemy
+        Logically, you only want to chase if the player is inbetween
+        the move and the attack radius
+        */
+            transform.position = Vector3.MoveTowards(transform.position,
+                                                    target.position,
+                                                    moveSpeed * Time.deltaTime);
+        }
+    }
+}
